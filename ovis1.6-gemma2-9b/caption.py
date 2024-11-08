daba6bc1d191a7d8acbdd987e7c57c23438684a2
PsychoLogicAu <psycho.logic.au@gmail.com>
2024-11-08 22:52:48 +1100

    ovis1.6-gemma2-9b working with Int4 quantized model

--- ovis1.6-gemma2-9b/caption.py
+++ ovis1.6-gemma2-9b/caption.py
@@ -1,18 +1,28 @@
 import torch
 from PIL import Image
+from transformers import GenerationConfig
+from auto_gptq.modeling import OvisGemma2GPTQForCausalLM
 import os
 import imghdr
-from ovis.serve.runner import RunnerArguments, OvisRunner
 
-checkpoint = "AIDC-AI/Ovis1.6-Gemma2-9B"
-runner_args = RunnerArguments(model_path=checkpoint)
-runner = OvisRunner(runner_args)
+# load model
+load_device = "cuda:0" # customize load device
+checkpoint = "AIDC-AI/Ovis1.6-Gemma2-9B-GPTQ-Int4"
+model = OvisGemma2GPTQForCausalLM.from_quantized(
+    checkpoint,
+    device=load_device,
+    trust_remote_code=True,
+)
+model.model.generation_config = GenerationConfig.from_pretrained(checkpoint)
+text_tokenizer = model.get_text_tokenizer()
+visual_tokenizer = model.get_visual_tokenizer()
 
 # load query string from the text file /data/query.txt
 with open("/data/query.txt", "r") as f:
     query = f"{f.read()}"
 
 print("Query:", query)
+query = f'<image>\n{query}'
 
 # iterate over the files in "/data/images" directory, and add detected image paths to img_path_list
 img_path_list = []
@@ -36,11 +46,31 @@ for img_path in img_path_list:
 
     image = Image.open(img_path)
 
-    generation = runner.run([image, query])
-    response = generation["output"]
+    # format conversation
+    prompt, input_ids, pixel_values = model.preprocess_inputs(query, [image])
+    attention_mask = torch.ne(input_ids, text_tokenizer.pad_token_id)
+    input_ids = input_ids.unsqueeze(0).to(device=model.device)
+    attention_mask = attention_mask.unsqueeze(0).to(device=model.device)
+    pixel_values = [pixel_values.to(dtype=visual_tokenizer.dtype, device=visual_tokenizer.device)]
 
-    print(f"{img_path}: {response}")
+    # generate output
+    with torch.inference_mode():
+        gen_kwargs = dict(
+            max_new_tokens=1024,
+            do_sample=False,
+            top_p=None,
+            top_k=None,
+            temperature=None,
+            repetition_penalty=None,
+            eos_token_id=model.generation_config.eos_token_id,
+            pad_token_id=text_tokenizer.pad_token_id,
+            use_cache=True
+        )
+        output_ids = model.generate(input_ids, pixel_values=pixel_values, attention_mask=attention_mask, **gen_kwargs)[0]
+        response = text_tokenizer.decode(output_ids, skip_special_tokens=True)
+       
+        print(f"{img_path}: {response}")
 
-    # Write the response to the output file
-    with open(output_path, "w") as f:
-        f.write(response)
+        # Write the response to the output file
+        with open(output_path, "w") as f:
+            f.write(response)
