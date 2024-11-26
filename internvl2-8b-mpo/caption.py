efd7476b44783e96520e06deaabedb8cee75eebb
PsychoLogicAu <psycho.logic.au@gmail.com>
2024-11-26 22:07:38 +1100

    added internvl2_8b_mpo

---
+++ internvl2-8b-mpo/caption.py
@@ -0,0 +1,167 @@
+import imghdr
+import numpy as np
+import os
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from torchvision.transforms.functional import InterpolationMode
+from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_STD = (0.229, 0.224, 0.225)
+
+
+def build_transform(input_size):
+    MEAN, STD = IMAGENET_MEAN, IMAGENET_STD
+    transform = T.Compose(
+        [
+            T.Lambda(lambda img: img.convert("RGB") if img.mode != "RGB" else img),
+            T.Resize((input_size, input_size), interpolation=InterpolationMode.BICUBIC),
+            T.ToTensor(),
+            T.Normalize(mean=MEAN, std=STD),
+        ]
+    )
+    return transform
+
+
+def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
+    best_ratio_diff = float("inf")
+    best_ratio = (1, 1)
+    area = width * height
+    for ratio in target_ratios:
+        target_aspect_ratio = ratio[0] / ratio[1]
+        ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+        if ratio_diff < best_ratio_diff:
+            best_ratio_diff = ratio_diff
+            best_ratio = ratio
+        elif ratio_diff == best_ratio_diff:
+            if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                best_ratio = ratio
+    return best_ratio
+
+
+def dynamic_preprocess(
+    image, min_num=1, max_num=12, image_size=448, use_thumbnail=False
+):
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+    # calculate the existing image aspect ratio
+    target_ratios = set(
+        (i, j)
+        for n in range(min_num, max_num + 1)
+        for i in range(1, n + 1)
+        for j in range(1, n + 1)
+        if i * j <= max_num and i * j >= min_num
+    )
+    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+    # find the closest aspect ratio to the target
+    target_aspect_ratio = find_closest_aspect_ratio(
+        aspect_ratio, target_ratios, orig_width, orig_height, image_size
+    )
+    # calculate the target width and height
+    target_width = image_size * target_aspect_ratio[0]
+    target_height = image_size * target_aspect_ratio[1]
+    blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+    # resize the image
+    resized_img = image.resize((target_width, target_height))
+    processed_images = []
+    for i in range(blocks):
+        box = (
+            (i % (target_width // image_size)) * image_size,
+            (i // (target_width // image_size)) * image_size,
+            ((i % (target_width // image_size)) + 1) * image_size,
+            ((i // (target_width // image_size)) + 1) * image_size,
+        )
+        # split the image
+        split_img = resized_img.crop(box)
+        processed_images.append(split_img)
+    assert len(processed_images) == blocks
+    if use_thumbnail and len(processed_images) != 1:
+        thumbnail_img = image.resize((image_size, image_size))
+        processed_images.append(thumbnail_img)
+    return processed_images
+
+
+def load_image(image_file, input_size=448, max_num=12):
+    image = Image.open(image_file).convert("RGB")
+    transform = build_transform(input_size=input_size)
+    images = dynamic_preprocess(
+        image, image_size=input_size, use_thumbnail=True, max_num=max_num
+    )
+    pixel_values = [transform(image) for image in images]
+    pixel_values = torch.stack(pixel_values)
+    return pixel_values
+
+
+# If you want to load a model using multiple GPUs, please refer to the `Multiple GPUs` section.
+checkpoint = "OpenGVLab/InternVL2-8B-MPO"
+quantization_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    llm_int8_enable_fp32_cpu_offload=True,
+    llm_int8_skip_modules=[
+        "mlp1.0",
+        "mlp1.1",
+        "mlp1.3",
+        "vision_model.embeddings",
+        "vision_model.encoder",
+    ],
+)
+device_map = "auto"
+model = AutoModel.from_pretrained(
+    checkpoint,
+    torch_dtype=torch.float16,
+    quantization_config=quantization_config,
+    device_map=device_map,
+    low_cpu_mem_usage=True,
+    use_flash_attn=True,
+    trust_remote_code=True,
+).eval()
+
+# Write the model structure to a file
+model_structure_file = f"/data/debug/{checkpoint}/model_structure.txt"
+os.makedirs(os.path.dirname(model_structure_file), exist_ok=True)
+with open(model_structure_file, "w") as file:
+    file.write(str(model))
+
+tokenizer = AutoTokenizer.from_pretrained(
+    checkpoint, trust_remote_code=True, use_fast=False
+)
+
+# load query string from the text file /data/query.txt
+with open("/data/query.txt", "r") as f:
+    query = f"<image>\n{f.read()}"
+
+print("Query:", query)
+
+# iterate over the files in "/data/images" directory, and add detected image paths to img_path_list
+img_path_list = []
+for root, dirs, files in os.walk("/data/images"):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if imghdr.what(file_path) is not None:
+            img_path_list.append(file_path)
+
+output_dir = f"/data/output/{checkpoint}"
+# Create the output directory if required
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for img_path in img_path_list:
+    output_path = f"{output_dir}/{img_path.split('/')[-1].split('.')[0]}.txt"
+
+    # If the output file already exists, skip the image
+    if os.path.exists(output_path):
+        continue
+
+    # set the max number of tiles in `max_num`
+    max_tiles = 12
+    pixel_values = load_image(img_path, max_num=max_tiles).to(torch.float16)
+    generation_config = dict(max_new_tokens=1024, do_sample=True)
+
+    response = model.chat(tokenizer, pixel_values, query, generation_config)
+
+    print(f"{img_path}: {response}")
+
+    # Write the response to the output file
+    with open(output_path, "w") as f:
+        f.write(response)
